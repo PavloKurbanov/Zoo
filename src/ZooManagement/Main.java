c4443158dffff200c9a230e9338a9fab29ce5a0f
MacOS <kurbanovpavlo@gmail.com>
2025-12-16 01:10:09 +0200

    Перевірка связі

--- src/ZooManagement/Main.java
+++ src/ZooManagement/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Zoo started!");
         System.out.println("Нарешті сука вдалось!");
+        System.out.println("Перевірка св'язі");
     }
 
 }
